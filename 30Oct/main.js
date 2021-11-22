332d8297c07e3e92bbdeee887cca4235472f65d9
Duygu-Yalman <duygu.yalman@outlook.com>
2021-11-22 22:37:42 +0300

    last

--- 30Oct/main.js
+++ 30Oct/main.js
@@ -1,5 +1,11 @@
 var date = new Date();
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-var milliseconds = date.milliseconds();
+var hours,minutes,seconds,milliseconds;
+setInterval(function(){
+    hours = date.getHours();
+    minutes = date.getMinutes();
+    seconds = date.getSeconds();
+    milliseconds = date.milliseconds();
+    console.log(hours+":"+minutes+":"+seconds+":"+milliseconds);
+},10);
+
+
