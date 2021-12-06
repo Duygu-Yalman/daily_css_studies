e9ea132029625574231ad0a5710ec418b6d12e07
Duygu-Yalman <duygu.yalman@outlook.com>
2021-12-06 23:07:36 +0300

    javascript exercises

---
+++ 22Nov/hasan.js
@@ -0,0 +1,17 @@
+const firstNumber = 14654661
+const secondNumber = 132134661
+const multiplyResult =  firstNumber * secondNumber
+const finalResult = multiplyResult ** 3654654
+//console.log("result =",finalResult)
+const rest = finalResult % 3
+const booleanResult1  = rest === 1
+const booleanResult2  = rest === 2
+
+
+if(booleanResult1){
+    console.log("kalan bir")
+}else if(booleanResult2){
+    console.log("kalan iki")
+}else{
+    console.log("kalan 0")
+}
