f75ec70b7a8e1e2a30fce42d193c98c51080fa86
Duygu-Yalman <duygu.yalman@outlook.com>
2021-12-10 21:04:55 +0300

    numbers exercises

--- Javascript Exercises/script.js
+++ Javascript Exercises/script.js
@@ -200,3 +200,33 @@
     // console.log(birthday.getTime);
     // console.log(Date.now());
 
+//Numbers 
+
+let val;
+
+//val = Number('10'); 
+//val = parseFloat('10.5');
+//val = parseInt('10');
+//val = parseInt('a10a');
+//val = isNaN('10');
+//val = isNaN('a10');
+
+//var num = 10.123456789;
+//val = num.toPrecision(6);
+//val = num.toFixed(2);
+
+val = Math.PI;
+val = Math.round(2.4);
+val = Math.ceil(2.6);
+val = Math.floor(2.4);
+val = Math.sqrt(64);
+val = Math.pow(2,4);
+val = Math.abs(-100);
+val = Math.min(1,2,3,2,4,9);
+val = Math.max(1,25,289,2,6);
+val = Math.random();
+val = Math.random()*10;
+
+
+console.log(val);
+console.log(typeof val);
